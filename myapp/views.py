35f2c51eda7094156a8fa40ed19d64bec3f057c0
ashutosh4398 <ashutoshdhondkar@gmail.com>
2024-04-13 19:08:13 +0530

    added aggregate queries

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
+import datetime
 from django.shortcuts import redirect, render
+from django.db.models import Sum
 
 from .forms import ExpenseForm
 from .models import Expense
@@ -14,7 +16,39 @@ def index(request):
             return redirect("index")
 
     expenses = Expense.objects.all()
-    context = {"form": form, "expenses": expenses}
+    # logic to calculate 365 days expenses
+    get_date = lambda x: datetime.date.today() - datetime.timedelta(days=x)
+
+    daily_sums = (
+        Expense.objects.filter()
+        .values("date_created__date")
+        .order_by("date_created__date")
+        .annotate(sum=Sum("amount"))
+    )
+
+    categorical_expenses = (
+        Expense.objects
+        .values("category")
+        .order_by("category")
+        .annotate(amount=Sum('amount'))
+    )
+    print(daily_sums)
+    print(categorical_expenses)
+    context = {
+        "form": form,
+        "expenses": expenses,
+        "agg": expenses.aggregate(sum=Sum("amount")),
+        "yearly_sum": expenses.filter(date_created__date__gte=get_date(365)).aggregate(
+            sum=Sum("amount")
+        )["sum"],
+        "monthlys_sum": expenses.filter(date_created__date__gte=get_date(30)).aggregate(
+            sum=Sum("amount")
+        )["sum"],
+        "weekly_sum": expenses.filter(date_created__date__gte=get_date(7)).aggregate(
+            sum=Sum("amount")
+        )["sum"],
+    }
+    print(context)
     return render(request, "myapp/index.html", context)
 
 
